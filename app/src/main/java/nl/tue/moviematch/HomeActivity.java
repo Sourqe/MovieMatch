5d2801c8b0bb96da2cbf5df3a8945f86bddc6ee9
Sourqe <nickvandewaterlaat@hotmail.com>
2018-03-07 14:52:28 +0100

    Added HomeActivity create MovieSearchFragment
    Immediately create MovieSearchFragment in HomeActivity

--- app/src/main/java/nl/tue/moviematch/HomeActivity.java
+++ app/src/main/java/nl/tue/moviematch/HomeActivity.java
@@ -1,13 +1,23 @@
 package nl.tue.moviematch;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        MovieSearchFragment fragment = new MovieSearchFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.home_layout, fragment);
+        transaction.commit();
     }
 }
