1987bae342135917277aa378a4f551757e024ce0
Sourqe <nickvandewaterlaat@hotmail.com>
2018-03-11 12:07:03 +0100

    HomeActivity now opens MovieSearchFragment

--- app/src/main/java/nl/tue/moviematch/HomeActivity.java
+++ app/src/main/java/nl/tue/moviematch/HomeActivity.java
@@ -1,23 +1,45 @@
 package nl.tue.moviematch;
 
+import android.support.v4.app.FragmentManager;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+
+
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
 public class HomeActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MovieSearchFragment fragment = new MovieSearchFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.home_layout, fragment);
-        transaction.commit();
+        setContentView(R.layout.activity_home);
+        button = findViewById(R.id.search_button);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        MovieSearchFragment movieSearchFragment = new MovieSearchFragment();
+        fragmentTransaction.add(R.id.fragment_container, movieSearchFragment);
+        fragmentTransaction.commit();
+
+        /* button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                MovieListFragment movieListFragment = new MovieListFragment();
+                fragmentTransaction.replace(R.id.fragment_container, movieListFragment);
+                fragmentTransaction.commit();
+            }
+        }); */
     }
 }
