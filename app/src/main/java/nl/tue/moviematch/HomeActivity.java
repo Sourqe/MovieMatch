eb4571c8575fa2f1ab2ad9c16efd139527e105e1
Sourqe <nickvandewaterlaat@hotmail.com>
2018-03-12 20:42:18 +0100

    Renamed layout stuff

--- app/src/main/java/nl/tue/moviematch/HomeActivity.java
+++ app/src/main/java/nl/tue/moviematch/HomeActivity.java
@@ -10,37 +10,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 
 public class HomeActivity extends AppCompatActivity {
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        button = (Button)findViewById(R.id.search_button);
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         MovieSearchFragment movieSearchFragment = new MovieSearchFragment();
-        fragmentTransaction.add(R.id.fragment_container, movieSearchFragment);
+        fragmentTransaction.add(R.id.movieSearchFragmentContainer, movieSearchFragment);
         fragmentTransaction.commit();
-
-        /*
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                MovieListFragment movieListFragment = new MovieListFragment();
-                fragmentTransaction.replace(R.id.fragment_container, movieListFragment);
-                fragmentTransaction.commit();
-            }
-        });*/
     }
 }
