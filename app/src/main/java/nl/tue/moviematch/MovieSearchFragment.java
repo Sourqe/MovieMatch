d2d1c624583b486041c3b647d2ca0fb5018a3160
Sourqe <nickvandewaterlaat@hotmail.com>
2018-03-12 20:45:25 +0100

    Added button functionality in MovieSearchFragment
    Clicking on the button opens MovieListFragment and displays the layout
    (list of movie). Still needs to be extended. Buttons in
    MovieListFragment also still need functionality.

--- app/src/main/java/nl/tue/moviematch/MovieSearchFragment.java
+++ app/src/main/java/nl/tue/moviematch/MovieSearchFragment.java
@@ -3,11 +3,20 @@ package nl.tue.moviematch;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
+import android.support.v4.app.FragmentManager;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.support.v4.app.Fragment;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class MovieSearchFragment extends Fragment {
@@ -24,10 +33,10 @@ public class MovieSearchFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_movie_search, container, false);
 
         // String arrays containing the dropdown menu items
-        String [] SPINNERLISTGENRE={"HORROR", "DRAMA", "ROMANCE", "ACTION", "COMEDY", "ALL"};
-        String [] SPINNERLISTYEAR={"1990s", "1980s", "1970s", "2000s", "2010s", "ALL"};
-        String [] SPINNERLISTLENGTH={"< 60 min", "< 120 min", "< 180 min", "> 180 min", "ALL"};
-        String [] SPINNERLISTRATING={"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "ALL"};
+        String[] SPINNERLISTGENRE = {"HORROR", "DRAMA", "ROMANCE", "ACTION", "COMEDY", "ALL"};
+        String[] SPINNERLISTYEAR = {"1990s", "1980s", "1970s", "2000s", "2010s", "ALL"};
+        String[] SPINNERLISTLENGTH = {"< 60 min", "< 120 min", "< 180 min", "> 180 min", "ALL"};
+        String[] SPINNERLISTRATING = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "ALL"};
 
         // One dropdown menu
         // Initialize spinner and connect it with spinner id
@@ -53,6 +62,18 @@ public class MovieSearchFragment extends Fragment {
         ArrayAdapter<String> adapterRating = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_dropdown_item_1line, SPINNERLISTRATING);
         adapterRating.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         betterSpinnerRating.setAdapter(adapterRating);
+
+        Button button = (Button) v.findViewById(R.id.search_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                MovieListFragment movieListFragment = new MovieListFragment();
+                fragmentTransaction.replace(R.id.movieListFragmentContainer, movieListFragment);
+                fragmentTransaction.commit();
+            }
+        });
 
         // Return the view
         return v;
