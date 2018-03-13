0a27cf814eb36baeccf6c8eea2fc7dfa007fa3cd
Sourqe <nickvandewaterlaat@hotmail.com>
2018-03-13 08:09:13 +0100

    Added functionality for theater search button

--- app/src/main/java/nl/tue/moviematch/TheaterSearchFragment.java
+++ app/src/main/java/nl/tue/moviematch/TheaterSearchFragment.java
@@ -3,70 +3,47 @@ package nl.tue.moviematch;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
+import android.support.v4.app.FragmentManager;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.support.v4.app.Fragment;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TheaterSearchFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
 public class TheaterSearchFragment extends Fragment {
-
-    private OnFragmentInteractionListener mListener;
 
     public TheaterSearchFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_theater_search, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        // Create new view
+        View v = inflater.inflate(R.layout.fragment_movie_search, container, false);
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        Button button = (Button) v.findViewById(R.id.search_theater);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                TheaterListFragment theaterListFragment = new TheaterListFragment();
+                fragmentTransaction.replace(R.id.theaterListFragmentContainer, theaterListFragment);
+                fragmentTransaction.commit();
+            }
+        });
+        // Return the view
+        return v;
     }
 }
