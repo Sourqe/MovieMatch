933447578cfa4d64cf66031b20890942a083bcdd
Ameya Rao <ameyarao98@gmail.com>
2018-03-17 20:56:06 +0100

    basic fragment layout

--- app/src/main/java/nl/tue/moviematch/TheaterActivity.java
+++ app/src/main/java/nl/tue/moviematch/TheaterActivity.java
@@ -1,20 +1,34 @@
 package nl.tue.moviematch;
-
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class TheaterActivity extends AppCompatActivity {
-
+public class TheaterActivity extends FragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theater);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        MovieSearchFragment movieSearchFragment = new MovieSearchFragment();
-        fragmentTransaction.add(R.id.theaterSearchFragmentContainer, movieSearchFragment);
-        fragmentTransaction.commit();
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.theaterSearchFragmentContainer) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            TheaterSearchFragment firstFragment = new TheaterSearchFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.theaterSearchFragmentContainer, firstFragment).commit();
+        }
     }
 }
