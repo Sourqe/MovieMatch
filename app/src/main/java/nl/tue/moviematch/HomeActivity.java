abd8e5bc9ca21a70300b87cd2e3d020850675794
Sourqe <nickvandewaterlaat@hotmail.com>
2018-03-25 00:28:47 +0100

    Cleaned up HomeActivity code
    Removed unnecessary code and added comments

--- app/src/main/java/nl/tue/moviematch/HomeActivity.java
+++ app/src/main/java/nl/tue/moviematch/HomeActivity.java
@@ -13,11 +13,23 @@ public class HomeActivity extends MainActivity implements MovieSearchFragment.Da
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        MovieSearchFragment movieSearchFragment = new MovieSearchFragment();
-        fragmentTransaction.add(R.id.movieSearchFragmentContainer, movieSearchFragment);
-        fragmentTransaction.commit();
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.movieSearchFragmentContainer) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+            MovieSearchFragment msFragment = new MovieSearchFragment();
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.movieSearchFragmentContainer, msFragment).commit();
+        }
     }
 
     @Override
