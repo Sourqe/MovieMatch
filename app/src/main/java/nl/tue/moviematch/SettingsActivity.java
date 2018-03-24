3a73a222e7c160627b4aef82be2307128c718b21
Sourqe <nickvandewaterlaat@hotmail.com>
2018-03-25 00:23:16 +0100

    Added working screen for settings
    Created a new fragment for the settings. Would rather do it directly
    from activity, but nothing worked when I tried. Have to revisit this
    problem. If we can't solve it, use this method. Otherwise, go with the
    direct method from the activity without any fragment.

--- app/src/main/java/nl/tue/moviematch/SettingsActivity.java
+++ app/src/main/java/nl/tue/moviematch/SettingsActivity.java
@@ -8,5 +8,21 @@ public class SettingsActivity extends MainActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.settings_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+            SettingsFragment sFragment = new SettingsFragment();
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.settings_container, sFragment).commit();
+        }
     }
 }
