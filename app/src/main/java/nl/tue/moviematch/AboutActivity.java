0ec7aed8e53edc0a543b4f425dc73dbd5f0fe88e
Sourqe <nickvandewaterlaat@hotmail.com>
2018-03-25 09:31:46 +0200

    Added setContentViews to try to fix the problem.

--- app/src/main/java/nl/tue/moviematch/AboutActivity.java
+++ app/src/main/java/nl/tue/moviematch/AboutActivity.java
@@ -8,5 +8,6 @@ public class AboutActivity extends MainActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
     }
 }
