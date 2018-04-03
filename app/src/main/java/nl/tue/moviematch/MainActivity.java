aea1b732d71ae4ff765eb991d8bb20da0d11e152
Sourqe <nickvandewaterlaat@hotmail.com>
2018-04-03 19:19:01 +0200

    Added colour for top bar, removed theater XML, more
    added new dutch text as well

--- app/src/main/java/nl/tue/moviematch/MainActivity.java
+++ app/src/main/java/nl/tue/moviematch/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -44,6 +46,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         // Get the suppport action bar and enable it
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Changing statusbar color
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.darkergreenish));
+        }
     }
 
     @Override
